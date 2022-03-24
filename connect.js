93e6fa69935fd5394673f8b6dc6e0edb918a4b5c
YOKESH K S <ksyokesh98@gmail.com>
2022-03-24 23:53:22 +0530

    feat(monogodb connect): mongodb connected, data is fetched (#1)

---
+++ connect.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectToDB = async () => {
+    mongoose.connect("mongodb+srv://yokesh:yokesh123@apidev.e8ljl.mongodb.net/moviesuggest?retryWrites=true&w=majority", {
+        useNewUrlParser: true,
+     // useFindAndModify: false,
+        useUnifiedTopology: true,
+    });
+}
+
+module.exports = connectToDB;
